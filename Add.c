dc23c62483726348c9b1423171053b4477c85e5b
Helektra <960421sean@gmail.com>
2025-12-02 14:26:38 +0800

    1202 - 2

--- Add.c
+++ Add.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 
-int add(int a , int b) {
-    return a + b;
+int add(int a , int b,int c) {
+    return a + b + c;
 }
 
 int main(){
-    int a , b;
-    printf("Enter two integers: ");
-    scanf("%d %d", &a, &b);
+    int a , b, c;
+    printf("Enter three integers: ");
+    scanf("%d %d %d", &a, &b, &c);
 
-    printf("Sum: %d\n", add(a, b));
+    printf("Sum: %d\n", add(a, b,c));
     return 0;
 }
