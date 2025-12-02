a961e09d4a36646a80fb1d1c63009ab841e64554
Helektra <960421sean@gmail.com>
2025-12-02 14:50:21 +0800

    1202 - 3

--- minus.c
+++ minus.c
@@ -4,11 +4,16 @@ int minus(int a , int b) {
     return a - b;
 }
 
+int add(int a , int b) {
+    return a + b;
+}
+
 int main(){
     int a , b;
     printf("Enter two integers: ");
     scanf("%d %d", &a, &b);
 
     printf("Sum: %d\n", minus(a, b));
+    printf("Difference: %d\n", add(a, b));
     return 0;
 }
